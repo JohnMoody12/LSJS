4650aa6e4e4aeb4e12f00f9120b912da7b79fb76
John Moody <ec2-user@ip-172-31-10-254.eu-north-1.compute.internal>
2023-12-12 01:00:05 +0000

    Initial commit

---
+++ node.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const os = require('os');
+
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
+
+console.log(__dirname);
+console.log(__filename);
+
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
