40a187c5d5d940b99360f363da7ac01d0250b89b
John Moody <ec2-user@ip-172-31-10-254.eu-north-1.compute.internal>
2023-12-12 02:33:25 +0000

    Calculator updated

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,27 +1,55 @@
 const readline = require("readline-sync");
 
+function prompt(message){
+  console.log(`=> ${message}`);
+}
+
+function invalidNumber(number){
+  return number.trimStart() === ''|| Number.isNaN(Number(number));
+}
+
+
 console.log("Welcome!");
-console.log("First number?");
+prompt("First number?")
 let n1 =readline.question();
 
-console.log("Second number?");
+while(invalidNumber(n1)){
+  prompt("Not a valid number");
+  n1 = readline.question();
+}
+
+prompt("Second number?");
 let n2 =readline.question();
 
-console.log("Operation?\n1)Add\n2)Sub\n3)Mul\n4)Div");
+while(invalidNumber(n2)){
+  prompt("Not a valid number");
+  n2 = readline.question();
+}
+
+prompt("Operation?\n1)Add\n2)Sub\n3)Mul\n4)Div");
 let op =readline.question();
+while(!['1','2','3','4'].includes(op)){
+  prompt("Not a valid operation - 1, 2, 3, or 4");
+  op = readline.question();
+}
 
 let output;
 
 //test comment
-function calc(n1,n2,op){
-  if(op=="1"){
-    return Number(n1)+Number(n2);
-  }else if (op === "2"){
-    return Number(n1)-Number(n2);
-  }else if (op === "3"){
-    return Number(n1)*Number(n2);
-  }else if (op === "4"){
-    return Number(n1)/Number(n2);
-  }
+
+  switch(op){
+    case "1":
+      output = Number(n1)+Number(n2);
+      break;
+    case "2":
+      output = Number(n1)-Number(n2);
+      break;
+    case "3":
+      output = Number(n1)*Number(n2);
+      break;
+    case "4":
+      output = Number(n1)/Number(n2);
+      break;  
 }
-console.log(`The result is ${calc(n1,n2,op)}`);
+
+console.log(`The result is ${output}`);
